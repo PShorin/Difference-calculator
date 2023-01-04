a9ec24475b1b570b955a29513de664e14bf02a2f
Pavel Shorin <pavel.shorin98@gmail.com>
2023-01-04 14:21:48 +0700

    Update gendiff.js, added format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,7 @@ import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1');
+  .version('0.1')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
