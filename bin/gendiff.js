fe5fc4314c7bd67bde3a913088e7a20a6328269c
Pavel Shorin <pavel.shorin98@gmail.com>
2023-01-10 14:32:41 +0700

    Updated gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1')
   .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     const diff = gendiff(filepath1, filepath2)
     console.log(diff);
