8648421d4a59d42635625fafc7abff2bd2e45404
Pavel Shorin <pavel.shorin98@gmail.com>
2023-01-23 19:08:33 +0700

    Update gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -16,6 +16,11 @@ let resultNested;
 let resultPlain;
 let resultJSON;
 
+const file1nestedJSON = getFixturesPath('file1nested.json');
+const file2nestedJSON = getFixturesPath('file2nested.json');
+const file1nestedYML = getFixturesPath('file1nested.yml');
+const file2nestedYML = getFixturesPath('file2nested.yml');
+
 beforeAll(() => {
   resultFlat = readFixture('resultFlat.txt');
   resultNested = readFixture('resultNested.txt');
@@ -23,38 +28,45 @@ beforeAll(() => {
   resultJSON = readFixture('resultJSON.txt');
 });
 
-test('Stylish, Flat YML and JSON files comparison', () => {
-  expect(gendiff('file1.json', 'file2.json')).toEqual(resultFlat);
-  expect(gendiff('file1.yml', 'file2.yml')).toEqual(resultFlat);
-  expect(gendiff('file1.yml', 'file2.json')).toEqual(resultFlat);
-  expect(gendiff('file1.json', 'file2.yml')).toEqual(resultFlat);
+test('1. Stylish, Flat YML and JSON files comparison', () => {
+  const file1JSON = getFixturesPath('file1.json');
+  const file2JSON = getFixturesPath('file2.json');
+  const file1YML = getFixturesPath('file1.yml');
+  const file2YML = getFixturesPath('file2.yml');
+
+  expect(gendiff(file1JSON, file2JSON)).toEqual(resultFlat);
+  expect(gendiff(file1YML, file2YML)).toEqual(resultFlat);
+  expect(gendiff(file1YML, file2JSON)).toEqual(resultFlat);
+  expect(gendiff(file1JSON, file2YML)).toEqual(resultFlat);
 });
 
-test('Stylish, Nested YML and JSON files comparison', () => {
-  expect(gendiff('file1nested.json', 'file2nested.json')).toEqual(resultNested);
-  expect(gendiff('file1nested.yml', 'file2nested.yml')).toEqual(resultNested);
-  expect(gendiff('file1nested.yml', 'file2nested.json')).toEqual(resultNested);
-  expect(gendiff('file1nested.json', 'file2nested.yml')).toEqual(resultNested);
+test('2. Stylish, Nested YML and JSON files comparison', () => {
+  expect(gendiff(file1nestedJSON, file2nestedJSON)).toEqual(resultNested);
+  expect(gendiff(file1nestedYML, file2nestedYML)).toEqual(resultNested);
+  expect(gendiff(file1nestedYML, file2nestedJSON)).toEqual(resultNested);
+  expect(gendiff(file1nestedJSON, file2nestedYML)).toEqual(resultNested);
 });
 
-test('Plain, Nested YML and JSON files comparison', () => {
-  expect(gendiff('file1nested.json', 'file2nested.json', 'plain')).toEqual(resultPlain);
-  expect(gendiff('file1nested.yml', 'file2nested.yml', 'plain')).toEqual(resultPlain);
-  expect(gendiff('file1nested.yml', 'file2nested.json', 'plain')).toEqual(resultPlain);
-  expect(gendiff('file1nested.json', 'file2nested.yml', 'plain')).toEqual(resultPlain);
+test('3. Plain, Nested YML and JSON files comparison', () => {
+  expect(gendiff(file1nestedJSON, file2nestedJSON, 'plain')).toEqual(resultPlain);
+  expect(gendiff(file1nestedYML, file2nestedYML, 'plain')).toEqual(resultPlain);
+  expect(gendiff(file1nestedYML, file2nestedJSON, 'plain')).toEqual(resultPlain);
+  expect(gendiff(file1nestedJSON, file2nestedYML, 'plain')).toEqual(resultPlain);
 });
 
-test('JSON, Nested YML and JSON files comparison', () => {
-  expect(gendiff('file1nested.json', 'file2nested.json', 'json')).toEqual(resultJSON);
-  expect(gendiff('file1nested.yml', 'file2nested.yml', 'json')).toEqual(resultJSON);
-  expect(gendiff('file1nested.yml', 'file2nested.json', 'json')).toEqual(resultJSON);
-  expect(gendiff('file1nested.json', 'file2nested.yml', 'json')).toEqual(resultJSON);
+test('4. JSON, Nested YML and JSON files comparison', () => {
+  expect(gendiff(file1nestedJSON, file2nestedJSON, 'json')).toEqual(resultJSON);
+  expect(gendiff(file1nestedYML, file2nestedYML, 'json')).toEqual(resultJSON);
+  expect(gendiff(file1nestedYML, file2nestedJSON, 'json')).toEqual(resultJSON);
+  expect(gendiff(file1nestedJSON, file2nestedYML, 'json')).toEqual(resultJSON);
 });
 
-test('Test Error (Wrong extension)', () => {
-  expect(() => { gendiff('resultFlat.txt', 'resultNested.txt', 'json'); }).toThrow('Extension txt is not supported');
+test('5. Test Error (Wrong extension)', () => {
+  const file1TXT = getFixturesPath('resultFlat.txt');
+  const file2TXT = getFixturesPath('resultNested.txt');
+  expect(() => { gendiff(file1TXT, file2TXT, 'json'); }).toThrow('Extension txt is not supported');
 });
 
-test('Test Error (Wrong formatter)', () => {
-  expect(() => { gendiff('file1nested.json', 'file2nested.json', 'jso'); }).toThrow('Format jso is not supported');
+test('6. Test Error (Wrong formatter)', () => {
+  expect(() => { gendiff(file1nestedJSON, file2nestedJSON, 'jso'); }).toThrow('Format jso is not supported');
 });
